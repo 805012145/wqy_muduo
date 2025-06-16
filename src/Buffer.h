7c6dd4a00c570c51588f0cc6e9ed3f4f14bebbca
wqy <805012145@qq.com>
2025-06-17 00:11:27 +0800

    feat:新增buffer代码

--- src/Buffer.h
+++ src/Buffer.h
@@ -1,9 +1,88 @@
 #pragma once
 
+#include <cstddef>
+#include <vector>
+#include <string>
 namespace mymuduo {
     namespace base {
         class Buffer {
-            
+        public:
+            static const size_t kCheapPrepend = 8;   // 用于记录空间大小
+            static const size_t kInitialSize = 1024; // 初始缓冲区大小
+            explicit Buffer(size_t initialSize = kInitialSize)
+             : buffer_(kCheapPrepend + kInitialSize)
+             , readerIndex_(kCheapPrepend)
+             , writerIndex_(kCheapPrepend) {
+
+             }
+            ~Buffer() {}
+            size_t readableBytes() const {
+                return writerIndex_ - readerIndex_; // 返回可读字节数
+            }
+
+            size_t writableBytes() const {
+                return buffer_.size() - writerIndex_; // 返回可写字节数
+            }
+
+            size_t prependableBytes() const {
+                return readerIndex_; // 返回预留字节数
+            }
+
+            const char* peek() const {
+                return begin() + readerIndex_; // 返回可读数据的起始地址
+            }
+
+            void retrieve(size_t len) {
+                if (len <= readableBytes()) {
+                    readerIndex_ += len;
+                } else {
+                    retrieveAll();
+                }
+            }
+
+            void retrieveAll() {
+                readerIndex_ = writerIndex_ = kCheapPrepend; // 重置读写指针
+            }
+
+            // 把OnMessage函数上报的buffer数据转成string类型数据返回
+            std::string retrieveAllAsString() {
+                return retrieveAsString(readableBytes());
+            }
+
+            std::string retrieveAsString(size_t len) {
+                std::string result(peek(), len);
+                retrieve(len); // 上面已经把缓冲区的可读数据读取出来，这里需要对缓冲区进行复位操作
+                return result; // 返回读取的数据
+            }
+
+            void ensureWritableBytes(size_t len) {
+                if (writableBytes() < len) {
+                    makeSpace(len); // 确保有足够的可写空间
+                }
+            }
+
+        private:
+            char *begin() {
+                return buffer_.data(); // 返回缓冲区的起始地址
+            }
+
+            const char *begin() const {
+                return buffer_.data(); // 返回缓冲区的起始地址
+            }
+
+            void makeSpace(size_t len) {
+                if (writableBytes() + prependableBytes() < len + kCheapPrepend) {
+                    buffer_.resize(writerIndex_ + len); // 扩展缓冲区大小
+                } else {
+                    size_t readable = readableBytes();
+                    std::copy(begin() + readerIndex_, begin() + writerIndex_, begin() + kCheapPrepend);
+                    readerIndex_ = kCheapPrepend;
+                    writerIndex_ = readerIndex_ + readable; // 更新读写指针
+                }
+            }
+            std::vector<char> buffer_; // 存储数据的缓冲区
+            size_t readerIndex_; // 读指针，指向下一个要读取的位置
+            size_t writerIndex_; // 写指针，指向下一个要写入的位置
         };
     }
 }
